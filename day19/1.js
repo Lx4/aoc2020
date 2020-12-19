64ec4a2a8e967b05bb5e88e35bb3f84c6450c1af
Franck FARCY <lx4@Francks-Air.home>
2020-12-19 11:39:44 +0100

    day19

---
+++ day19/1.js
@@ -0,0 +1,46 @@
+const data = require('fs').readFileSync("./sample", { encoding: "utf-8" })
+.trim();
+
+// init rules
+const lines = data.split("\n");
+const regex = /([0-9]+): (.*)/
+const rules = []
+
+// data to check
+const messages = require('./sampleentry')
+
+for (line of lines){
+    const match = line.match(regex);
+    rules[+match[1]] = match[2]
+}
+
+console.log(rules)
+
+// evaluate rules recursively starting at rules[0] to create a regex string
+// this regex string will be later use to check if entries are valid
+
+
+function createRegex(str, depth=0){
+    // end case
+    if (['a', 'b'].includes(str)) return str
+    const matchOr = str.match(/(.*)\|(.*)/)
+    if (matchOr){
+        return '(' + createRegex(matchOr[1], depth + 1) + '|' + createRegex(matchOr[2], depth + 1) + ')'
+    }
+    rulesMatched = str.trim().split(' ')
+    let strFound = ''
+    for (rule of rulesMatched) {
+        strFound += createRegex(rules[+rule], depth + 1)
+    }
+    return strFound
+}
+
+console.log(messages)
+const reg = '^'+createRegex(rules[0])+'$'
+console.log(reg)
+
+
+const numMatches  = messages.map((message) => message.match(reg) ? 1 : 0).reduce((a,b) => a + b)
+console.log(numMatches)
+
+// 314 too low 377 too high
